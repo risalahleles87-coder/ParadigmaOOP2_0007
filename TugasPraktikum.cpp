c03ce612dd871a470361148bd33d29cbd00ce68d
risalahlele87-coder <risalahleles87@gmail.com>
2026-05-18 13:00:32 +0700

    Membuat abstract class rekening bank

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -2,3 +2,27 @@
 #include <iomanip>
 using namespace std;
 
+// Abstract Base Class
+class RekeningBank {
+protected:
+    string namaNasabah;
+    double saldo;
+
+public:
+    RekeningBank(string nama, double saldoAwal) {
+        namaNasabah = nama;
+        saldo = saldoAwal;
+    }
+
+    // Pure Virtual Function
+    virtual void potongAdmin() = 0;
+
+    void tampilkanSaldo() {
+
+        cout << fixed << setprecision(0);
+
+        cout << "Nasabah : " << namaNasabah << endl;
+        cout << "Saldo   : Rp " << saldo << endl;
+        cout << "-----------------------------" << endl;
+    }
+};
